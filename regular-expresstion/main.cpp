0566e6c4f08c60cbfadc540b6f1042a4c73ccffb
wrericsson <rui-wang2002@163.com>
2017-05-03 14:54:46 +0800

    add parser func
    Signed-off-by: wrericsson <rui-wang2002@163.com>

--- regular-expresstion/main.cpp
+++ regular-expresstion/main.cpp
@@ -18,11 +18,24 @@ using namespace std;
 int main()  
 {  
     //例1：从<sip:tom@172.18.1.133>中提取tom  
-    const char* url = "<sip:tom@172.18.1.133>";  
-    char uri[10] = {0};  
-    sscanf(url, "%*[^:]:%[^@]", uri);  
-    cout << uri << endl;  
+    //const char* url = "<sip:tom@172.18.1.133>";  
+    //char uri[10] = {0};  
+    //sscanf(url, "%*[^:]:%[^@]", uri);  
+    //cout << uri << endl;  
+ 
+    const char* url = "https://www.baidu.com/";  
+    char uri_0[30] = {0};  
+    char uri_1[30] = {0};  
+    char uri_2[30] = {0};  
+    char uri_3[30] = {0};  
+    sscanf(url, "%[^://]://%[^.].%[^.].%[^/]/", uri_0,uri_1,uri_2,uri_3);  
+    cout << uri_0 << endl;  
+    cout << uri_1 << endl;  
+    cout << uri_2 << endl;  
+    cout << uri_3<< endl;  
   
+
+
     //例2：从iios/12DDWDFF@122中提取 12DDWDFF  
     const char* s = "iios/12DDWDFF@122";  
     char buf[20];  
